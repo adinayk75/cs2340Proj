c3006fb48c2e0b39d927f573d06af9960c879bd6
Sowmiyan Nattamai Vijayakumar <svijayakumar37@gatech.edu>
2024-10-05 17:06:06 -0400

    Added login/create account

--- Project/TravelApplication/app/src/main/java/com/example/travelapplication/view/LoginActivity.java
+++ Project/TravelApplication/app/src/main/java/com/example/travelapplication/view/LoginActivity.java
@@ -10,39 +10,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.databinding.library.baseAdapters.BR;
-import androidx.lifecycle.ViewModelProvider;
-import com.example.travelapplication.viewmodel.MainViewModel;
 
 import com.example.travelapplication.R;
-import com.example.travelapplication.databinding.ActivityMainBinding;
-import com.example.travelapplication.viewmodel.MainViewModel;
 
-public class MainActivity extends AppCompatActivity {
-    private Button startButton;
+public class LoginActivity extends AppCompatActivity {
+    private Button createAccountButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        MainViewModel viewModel = new ViewModelProvider(this).get(MainViewModel.class);
-        binding.setVariable(BR.viewmodel, viewModel);
-        binding.setLifecycleOwner(this);
+        setContentView(R.layout.activity_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        // Start button -> Log in page
-        startButton=findViewById(R.id.startButton);
-        startButton.setOnClickListener(new View.OnClickListener() {
+        // Create Account Button -> Create Account Page
+        createAccountButton=findViewById(R.id.createAccountButton);
+        createAccountButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                Intent intent = new Intent(LoginActivity.this, CreateAccountActivity.class);
                 startActivity(intent);
             }
         });
