b3200bf50c502c63256c5532410adc25152ef362
Sowmiyan Nattamai Vijayakumar <svijayakumar37@gatech.edu>
2024-10-05 21:56:34 -0400

    Finished login/create screen layout`

--- Project/TravelApplication/app/src/main/java/com/example/travelapplication/view/CreateAccountActivity.java
+++ Project/TravelApplication/app/src/main/java/com/example/travelapplication/view/CreateAccountActivity.java
@@ -1,15 +1,23 @@
 package com.example.travelapplication.view;
 
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
 import com.example.travelapplication.R;
 
+
 public class CreateAccountActivity extends AppCompatActivity {
+    private Button backToLoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +28,15 @@ public class CreateAccountActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        backToLoginButton=findViewById(R.id.loginCreateButton);
+        backToLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CreateAccountActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
