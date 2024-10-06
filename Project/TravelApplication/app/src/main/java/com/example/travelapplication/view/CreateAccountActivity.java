186fcafb185313e82494ec8b02bed89c678eff96
Sowmiyan Nattamai Vijayakumar <svijayakumar37@gatech.edu>
2024-10-06 13:31:14 -0400

    Made firebase auth sign in work.

--- Project/TravelApplication/app/src/main/java/com/example/travelapplication/view/CreateAccountActivity.java
+++ Project/TravelApplication/app/src/main/java/com/example/travelapplication/view/CreateAccountActivity.java
@@ -3,21 +3,41 @@ package com.example.travelapplication.view;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.travelapplication.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class CreateAccountActivity extends AppCompatActivity {
-    private Button backToLoginButton;
+    private Button backToLoginButton, registerButton;
+    private TextInputEditText editTextUsername, editTextPassword;
+    private FirebaseAuth mAuth;
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Toast.makeText(CreateAccountActivity.this, "Already Logged!", Toast.LENGTH_SHORT).show();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,13 +50,66 @@ public class CreateAccountActivity extends AppCompatActivity {
             return insets;
         });
 
+        // Login -> Login page button
         backToLoginButton=findViewById(R.id.loginCreateButton);
         backToLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(CreateAccountActivity.this, LoginActivity.class);
                 startActivity(intent);
+                finish();
             }
         });
+
+        // Username/Pass stuff
+        editTextUsername = findViewById(R.id.createUsername);
+        editTextPassword = findViewById(R.id.createPassword);
+        registerButton = findViewById(R.id.registerButton);
+        mAuth = FirebaseAuth.getInstance();
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String username, password;
+
+                username = String.valueOf(editTextUsername.getText()) + "@travelista.com";
+                password = String.valueOf(editTextPassword.getText());
+
+
+                if (TextUtils.isEmpty(username)) {
+                    Toast.makeText(CreateAccountActivity.this, "Enter Username", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(CreateAccountActivity.this, "Enter Password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                mAuth.createUserWithEmailAndPassword(username, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(CreateAccountActivity.this, "Account Created.",
+                                        Toast.LENGTH_SHORT).show();
+
+                                Intent intent = new Intent(CreateAccountActivity.this, Destination.class);
+                                startActivity(intent);
+                                finish();
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Toast.makeText(CreateAccountActivity.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                );
+
+
+            }
+        });
+
     }
 }
--- Project/TravelApplication/app/src/main/java/com/example/travelapplication/view/LoginActivity.java
+++ Project/TravelApplication/app/src/main/java/com/example/travelapplication/view/LoginActivity.java
@@ -2,23 +2,47 @@ package com.example.travelapplication.view;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.travelapplication.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+    private TextInputEditText editTextUsername, editTextPassword;
+    private FirebaseAuth mAuth;
+    private ProgressBar progressBar;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(LoginActivity.this, Destination.class);
+            startActivity(intent);
+            finish();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mAuth = FirebaseAuth.getInstance();
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
@@ -27,6 +51,9 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        editTextUsername = findViewById(R.id.username);
+        editTextPassword = findViewById(R.id.password);
 
         // Create Account Button -> Create Account Page
         Button createAccountButton = findViewById(R.id.createAccountButton);
@@ -39,6 +66,48 @@ public class LoginActivity extends AppCompatActivity {
         });
 
         Button loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String username, password;
+                username = String.valueOf(editTextUsername.getText()) + "@travelista.com";
+                password = String.valueOf(editTextPassword.getText());
+
+                if (TextUtils.isEmpty(username)) {
+                    Toast.makeText(LoginActivity.this, "Enter Username", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(LoginActivity.this, "Enter Password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                mAuth.signInWithEmailAndPassword(username, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+
+                                    Intent intent = new Intent(LoginActivity.this, Destination.class);
+                                    startActivity(intent);
+                                    finish();
+
+                                    Toast.makeText(LoginActivity.this, "Log in successful",
+                                            Toast.LENGTH_SHORT).show();
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+
+        /*
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -61,6 +130,7 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         });
+         */
 
     }
 }
