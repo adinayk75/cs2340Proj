4b861d3be241ede3882787fbe6f15640d881f977
Git-haan <ishaan.reddy5@gmail.com>
2024-10-12 13:19:27 -0400

    feat: navbar func

--- Project/TravelApplication/app/src/main/java/com/example/travelapplication/view/Accommodation.java
+++ Project/TravelApplication/app/src/main/java/com/example/travelapplication/view/Accommodation.java
@@ -1,14 +1,18 @@
 package com.example.travelapplication.view;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.travelapplication.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Accommodation extends AppCompatActivity {
 
@@ -17,10 +21,43 @@ public class Accommodation extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_accommodation);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.accommodation), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Navigation Bar
+
+        BottomNavigationView navigationView = findViewById(R.id.bottomNavigationView);
+
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (id == R.id.destination) {
+                    Intent intent = new Intent(Accommodation.this, Destination.class);
+                    startActivity(intent);
+                }
+                if (id == R.id.dining) {
+                    Intent intent = new Intent(Accommodation.this, Dining.class);
+                    startActivity(intent);
+                }
+                if (id == R.id.accommodation) {
+                    Intent intent = new Intent(Accommodation.this, Accommodation.class);
+                    startActivity(intent);
+                }
+                if (id == R.id.transportation) {
+                    Intent intent = new Intent(Accommodation.this, Transportation.class);
+                    startActivity(intent);
+                }
+                if (id == R.id.travel) {
+                    Intent intent = new Intent(Accommodation.this, Travel.class);
+                    startActivity(intent);
+                }
+
+                return false;
+            }
         });
     }
 }
