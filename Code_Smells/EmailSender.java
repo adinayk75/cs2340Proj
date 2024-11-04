19bb9c25564a642a110a40c19a1871ea29699590
adinayk75 <adinayk@gmail.com>
2024-11-03 23:35:50 -0500

    Fixed issues in EmailSender.java file in Code Smells #2

--- Code_Smells/EmailSender.java
+++ Code_Smells/EmailSender.java
@@ -4,9 +4,11 @@ public class EmailSender {
 
     private static final Logger logger = Logger.getLogger(EmailSender.class.getName());
 
+    private EmailSender(){}
+
     public static void sendEmail(String customerEmail, String subject, String message){
-        logger.info("Email to: " + customerEmail);
-        logger.info("Subject: " + subject);
-        logger.info("Body: " + message);
+        logger.log("Email to: " + customerEmail);
+        logger.log("Subject: " + subject);
+        logger.log("Body: " + message);
     }
 }
