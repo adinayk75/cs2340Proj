5d201685026d08208a05bb6f4b57a449f24b246d
rraina34 <rraina34@gatech.edu>
2024-11-10 21:49:00 -0500

    fixed the dining page

---
+++ Project/TravelApplication/app/src/main/java/com/example/travelapplication/model/DiningInfo.java
@@ -0,0 +1,50 @@
+package com.example.travelapplication.model;
+
+public class DiningInfo {
+    private String location;
+    private String time;
+    private String website;
+    private String name;
+
+    // Default constructor (needed for Firebase)
+    public DiningInfo() {}
+
+    public DiningInfo(String location, String time, String website, String name) {
+        this.location = location;
+        this.time = time;
+        this.website = website;
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
